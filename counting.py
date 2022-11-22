db5dd535f3d7fd738ca843854e2df0d642b96ef3
Johanna Liu <johanna.liu@mths.ca>
2022-11-22 15:34:48 +0000

    init

---
+++ counting.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Created by: Johanna Liu
+# Created on: Oct 2022
+# This program adds two numbers
+
+import random
+
+
+def main():
+
+    # process
+    value = input("Press Enter to print all the numbers between 1000-2000.")
+    for num in range(1000, 2001):
+        if num % 5 == 0 and num != 1000:
+            print("\n{0} ".format(num), end = "")
+        else:
+            print("{0} ".format(num), end = "")
+    print("")
+    print("\nDone.")
+
+if __name__ == "__main__":
+    main()
